8aa732a3d2a7c606afeac36ee359eaa7a7477e92
Ryan Rushton <ryan.rushton79@gmail.com>
2021-11-09 00:11:43 +1100

    More cleanup.

--- src/app/loadout-builder/filter/LockArmorAndPerks.tsx
+++ src/app/loadout-builder/filter/LockArmorAndPerks.tsx
@@ -143,7 +143,7 @@ export default memo(function LockArmorAndPerks({
               <LockedModIcon
                 key={getModRenderKey(mod, modCounts)}
                 mod={mod}
-                onClosed={() => onModClicked(mod)}
+                onModClicked={() => onModClicked(mod)}
               />
             ))}
           </div>
